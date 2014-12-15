72d72c1fb4068f21994fedbdf2bf6941a6e61a57
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-15 14:16:35 -0600

    Looping within a Loop

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,29 @@
     $http.get(url + "patient.drug.openfda.pharm_class_epc:\"nonsteroidal+anti-inflammatory+drug\"&count=patient.reaction.reactionmeddrapt.exact" )
     .success(function(data) {
       a.FDA = data.results;
-      console.log(data);
+      console.log(a.FDA);
+    })
+    .error(function(err) {
+      console.log(err);
+    })
+  })
+  .controller("FDABrandController", function($http) {
+    var a = this,
+    url = "https://api.fda.gov/drug/event.json?api_key=Xgd5lx8BAGRQThoeLjBRWRApQZxr6o9Qh87R3rbU&search=";
+    $http.get(url + "patient.drug.openfda.brand_name:\"tylenol\"&count=patient.reaction.reactionmeddrapt.exact")
+    .success(function(data) {
+      a.FDAB = data.results;
+      console.log(a.FDAB);
+    })
+    .error(function(err) {
+      console.log(err);
+    })
+  })
+  .controller("PaymentController", function($http) {
+    var a = this; $http.get("https://openpaymentsdata.cms.gov/resource/identified-research-payments-2013.json?dispute_status_for_publication=No")
+    .success(function(data) {
+      a.PCP = data;
+      console.log(a.PCP)
     })
     .error(function(err) {
       console.log(err);
