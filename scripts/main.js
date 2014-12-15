c7fb2096064e22067ed6e1111ab225ad0bc46f33
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-15 11:31:43 -0600

    FDI Call Success

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 (function() {
   "use strict";
   angular.module("myApp", [])
-  .controller("FDACallController", function($http))
+  .controller("FDACallController", function($http) {
+    var a = this,
+    url = "https://api.fda.gov/drug/event.json?api_key=Xgd5lx8BAGRQThoeLjBRWRApQZxr6o9Qh87R3rbU&search=";
+    $http.get(url + "patient.drug.openfda.pharm_class_epc:\"nonsteroidal+anti-inflammatory+drug\"&count=patient.reaction.reactionmeddrapt.exact" )
+    .success(function(data) {
+      a.FDA = data.results;
+      console.log(data);
+    })
+    .error(function(err) {
+      console.log(err);
+    })
+  })
 }());
