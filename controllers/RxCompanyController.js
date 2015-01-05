c1803bb794106ac52a095470a6384ff8b99e0061
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-05 13:02:01 -0600

    RxCo Controller Refactor

--- controllers/RxCompanyController.js
+++ controllers/RxCompanyController.js
@@ -1,12 +1,12 @@
 (function() {
   "use strict";
-  RxCompanyController.$inject = [ "$http", "$routeParams", "RxCompanyFactory" ];
-  function RxCompanyController($http, $routeParams, RxCompanyFactory) {
+  RxCompanyController.$inject = [ "$http", "$location", "$routeParams", "RxCompanyFactory" ];
+  function RxCompanyController($http, $location, $routeParams, RxCompanyFactory) {
     var a = this;
     RxCompanyFactory.getRxCo(function(data) {
-      a.Rx = data;
-      a.rxCoTotalPMT(a.Rx);
-      console.log(a.Rx)
+      a.RxCo = data;
+      a.rxCoTotalPMT(a.RxCo);
+      console.log(a.RxCo)
     })
     a.rxCoTotalPMT = function(phy) {
       var a = this;
@@ -14,6 +14,9 @@
       $(phy).each(function(index) {
         a.RxCoPMTTotal = a.RxCoPMTTotal + Number(phy[index].total_amount_of_payment_usdollars, 2);
       })
+    }
+    a.goToDrug = function(name_of_associated_covered_drug_or_biological1, applicable_manufacturer_or_applicable_gpo_making_payment_name) {
+      $location.path("/rxsearch/" + applicable_manufacturer_or_applicable_gpo_making_payment_name + "/" + name_of_associated_covered_drug_or_biological1)
     }
   }
   angular.module("myApp")
