44b6498384c4152fd266036d79d88b2e25099f58
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-06 15:15:25 -0600

    permutator for rxcui WIP

--- controllers/RxController.js
+++ controllers/RxController.js
@@ -3,7 +3,11 @@
   RxController.$inject = [ "$http", "$routeParams", "RxFactory" ];
   function RxController($http, $routeParams, RxFactory) {
     var a = this;
-    a.rxNorm = 0;
+    a.rxNorm = {
+      ready: 1,
+      aim: 0,
+      fire: 0
+    };
     RxFactory.getRx(function(data) {
       a.Rx = data;
       a.rxGenTotalPMT(a.Rx);
@@ -16,17 +20,17 @@
         a.RxPMTTotal = a.RxPMTTotal + Number(phy[index].total_amount_of_payment_usdollars, 2);
       })
     }
-    a.hyphenateNDC = function(ref) {
+    a.hyphenateNDC = function(ref, hyphen) {
       a.NDC = ref.ndc_of_associated_covered_drug_or_biological1;
       a.NDCArr = [];
-      for ( var i = 0; i < 12; i++ ) {
-        if (i === 4 || i === 9) {
+      for ( var i = 0; i < 13; i++ ) {
+        if (i === hyphen.labeler || i === hyphen.labeler + hyphen.productrx + 1) {
           a.NDCArr.push("-")
-        } else if (i < 4) {
+        } else if (i < hyphen.labeler) {
           a.NDCArr.push(String(a.NDC.charAt(i)));
-        } else if (i < 9) {
-          a.NDCArr.push(String(a.NDC.charAt(i - 1 )));
-        } else if (i < 12) {
+        } else if (i < hyphen.productrx + hyphen.labeler + 1) {
+          a.NDCArr.push(String(a.NDC.charAt(i - 1)));
+        } else if (i < 13) {
           a.NDCArr.push(String(a.NDC.charAt(i - 2)));
         }
       }
@@ -34,24 +38,36 @@
       return a.NDCArr;
     }
     a.validateRxCui = function(ref) {
-      var hyphen = {
-        b: 5,
-        c: 4,
-        d: 3
-      };
-      if (typeof  a.rxNorm === "undefined") {
-        console.log("error with 4-4-2");
-      } else {
-        console.log("success");
+      var hyphen = [{
+          labeler: 4,
+          productrx: 4,
+          package: 2
+        },
+        {
+          labeler: 5,
+          productrx: 4,
+          package: 1
+        },
+        {
+          labeler: 5,
+          productrx: 3,
+          package: 2
+        }
+      ];
+      for (var i = 0; i < 3; i++ ) {
+        a.getRxCui(ref, hyphen[i]);
       }
     }
-    a.getRxCui = function(ref) {
-      a.hyphenNDC = a.hyphenateNDC(ref);
+    a.getRxCui = function(ref, hyphen) {
+      a.hyphenNDC = a.hyphenateNDC(ref, hyphen);
       var url = "http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=";
       $http.get(url + a.hyphenNDC)
       .success(function(data) {
         a.rxNorm = data.idGroup.rxnormId;
-        console.log(a.rxNorm, data);
+        if (a.rxNorm !== undefined) {
+          console.log(a.rxNorm)
+          return a.rxNorm;
+        }
       })
       .error(function(err) {
         console.log(err);
