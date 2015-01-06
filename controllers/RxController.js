a038ee4e4494ceb15818d65ede30034b456a2b6a
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-06 13:52:00 -0600

    RxCui validate for 4-4-2 working

--- controllers/RxController.js
+++ controllers/RxController.js
@@ -3,10 +3,11 @@
   RxController.$inject = [ "$http", "$routeParams", "RxFactory" ];
   function RxController($http, $routeParams, RxFactory) {
     var a = this;
+    a.rxNorm = 0;
     RxFactory.getRx(function(data) {
       a.Rx = data;
       a.rxGenTotalPMT(a.Rx);
-      a.getRxCui(a.Rx[0]);
+      a.validateRxCui(a.Rx[0]);
       console.log(a.Rx)
     })
     a.rxGenTotalPMT = function(phy) {
@@ -32,14 +33,21 @@
       a.NDCArr = a.NDCArr.join("");
       return a.NDCArr;
     }
-    a.getRxCui = function(ref) {
-      a.hyphenNDC = a.hyphenateNDC(ref);
-      var url = "http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=",
-      style = {
+    a.validateRxCui = function(ref) {
+      var hyphen = {
         b: 5,
         c: 4,
         d: 3
-      } ;
+      };
+      if (typeof  a.rxNorm === "undefined") {
+        console.log("error with 4-4-2");
+      } else {
+        console.log("success");
+      }
+    }
+    a.getRxCui = function(ref) {
+      a.hyphenNDC = a.hyphenateNDC(ref);
+      var url = "http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=";
       $http.get(url + a.hyphenNDC)
       .success(function(data) {
         a.rxNorm = data.idGroup.rxnormId;
