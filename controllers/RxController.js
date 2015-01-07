79762c9a89e3869b21f8c9b0e8fa58322e2ffdbe
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-06 21:31:27 -0600

    RxCui WIP

--- controllers/RxController.js
+++ controllers/RxController.js
@@ -3,15 +3,11 @@
   RxController.$inject = [ "$http", "$routeParams", "RxFactory" ];
   function RxController($http, $routeParams, RxFactory) {
     var a = this;
-    a.rxNorm = {
-      ready: 1,
-      aim: 0,
-      fire: 0
-    };
+    a.rxNorm = {};
     RxFactory.getRx(function(data) {
       a.Rx = data;
       a.rxGenTotalPMT(a.Rx);
-      a.dehyphenateNDC(a.Rx[0]);
+      a.validateRxCui(a.Rx[0]);
       console.log(a.Rx)
     })
     a.rxGenTotalPMT = function(phy) {
@@ -20,12 +16,22 @@
         a.RxPMTTotal = a.RxPMTTotal + Number(phy[index].total_amount_of_payment_usdollars, 2);
       })
     }
-    a.dehyphenateNDC = function(ref) {
-      a.NDC = ref.ndc_of_associated_covered_drug_or_biological1;
-      console.log(/[-._:]/.test(a.NDC));
+    a.dehyphenateNDC = function(ref, hyphen) {
+      var hyphenated = ref,
+      nohyphen = hyphenated.split("-").join("");
+      return nohyphen, hyphen;
+    }
+    a.hyphenTester = function(ref) {
+      if (!/[-._:]/.test(ref)) {
+        console.log("hyphen test true")
+        return true;
+      } else {
+        console.log("hyphen test false");
+        return false;
+      }
     }
     a.hyphenateNDC = function(ref, hyphen) {
-      a.NDC = ref.ndc_of_associated_covered_drug_or_biological1;
+      a.NDC = a.dehyphenateNDC(ref, hyphen);
       a.NDCArr = [];
       for ( var i = 0; i < 13; i++ ) {
         if (i === hyphen.labeler || i === (hyphen.labeler + hyphen.productrx + 1)) {
@@ -38,49 +44,74 @@
           a.NDCArr.push(String(a.NDC.charAt(i - 2)));
         }
       }
-      a.NDCArr = a.NDCArr.join("");
-      return a.NDCArr;
     }
     a.validateRxCui = function(ref) {
-      var hyphen = [{
-          labeler: 4,
-          productrx: 4,
-          package: 2
-        },
-        {
-          labeler: 5,
-          productrx: 4,
-          package: 1
-        },
-        {
-          labeler: 5,
-          productrx: 3,
-          package: 2
-        }
-      ];
-      for (var i = 0; i < 3; i++) {
-        a.getRxCui(ref, hyphen[i]);
-        if (a.rxNorm !== "undefined") {
-          break;
-        }
-      }
+      a.NDC = ref.ndc_of_associated_covered_drug_or_biological1;
+      var hyphen = [
+    {
+      labeler: 4,
+      productrx: 4,
+      package: 2
+    },
+  {
+    labeler: 5,
+    productrx: 4,
+    package: 1
+  },
+{
+  labeler: 5,
+  productrx: 3,
+  package: 2
+}
+];
+if (a.hyphenTester(ref.ndc_of_associated_covered_drug_or_biological1)) {
+  console.log(ref.ndc_of_associated_covered_drug_or_biological1);
+  // for (var i = 0; i < 3; i++) {
+  //     a.getRxCui(ref, hyphen[i]);
+  //     if (a.rxNorm !== "undefined") {
+  //       break;
+  //     }
+  //   }
+} else {
+  a.preRxCui(ref.ndc_of_associated_covered_drug_or_biological1);
+}
+// for (var i = 0; i < 3; i++) {
+//   a.getRxCui(ref, hyphen[i]);
+//   if (a.rxNorm !== "undefined") {
+//     break;
+//   }
+// }
+}
+a.getRxCui = function(ref, hyphen) {
+  a.hyphenNDC = a.hyphenateNDC(ref, hyphen);
+  var url = "http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=";
+  $http.get(url + a.hyphenNDC)
+  .success(function(data) {
+    a.rxNorm = data.idGroup.rxnormId;
+    if (a.rxNorm !== "undefined") {
+      console.log(a.rxNorm)
+      return a.rxNorm;
     }
-    a.getRxCui = function(ref, hyphen) {
-      a.hyphenNDC = a.hyphenateNDC(ref, hyphen);
-      var url = "http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=";
-      $http.get(url + a.hyphenNDC)
-      .success(function(data) {
-        a.rxNorm = data.idGroup.rxnormId;
-        if (a.rxNorm !== undefined) {
-          console.log(a.rxNorm)
-          return a.rxNorm;
-        }
-      })
-      .error(function(err) {
-        console.log(err);
-      })
+  })
+  .error(function(err) {
+    console.log(err);
+  })
+}
+a.preRxCui = function(ref) {
+  var url = "http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=";
+  $http.get(url + ref)
+  .success(function(data) {
+    a.rxNorm = data.idGroup.rxnormId;
+    if (a.rxNorm !== "undefined") {
+      console.log(a.rxNorm)
+      return a.rxNorm;
     }
-  }
-  angular.module("myApp")
-  .controller  ("RxController", RxController)
+  })
+  .error(function(err) {
+    console.log(err);
+  })
+}
+}
+angular.module("myApp")
+.controller  ("RxController", RxController)
 }());
