350e1741cc1e310d8c36981f758999645f54db26
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-08 14:03:30 -0600

    Refacor RxCui controller

--- controllers/RxController.js
+++ controllers/RxController.js
@@ -3,7 +3,7 @@
   RxController.$inject = [ "$http", "$routeParams", "RxFactory" ];
   function RxController($http, $routeParams, RxFactory) {
     var a = this;
-    a.rxNorm = {};
+    a.rxNorm;
     RxFactory.getRx(function(data) {
       a.Rx = data;
       a.rxGenTotalPMT(a.Rx);
@@ -16,14 +16,15 @@
         a.RxPMTTotal = a.RxPMTTotal + Number(phy[index].total_amount_of_payment_usdollars, 2);
       })
     }
-    a.dehyphenateNDC = function(ref, hyphen) {
+    a.dehyphenateNDC = function(ref) {
       var hyphenated = ref,
-      nohyphen = hyphenated.split("-").join("");
-      return nohyphen, hyphen;
+      ref = hyphenated.split("-").join("");
+      a.dehyphenatedNDC = ref;
+      return a.dehyphenatedNDC;
     }
     a.hyphenTester = function(ref) {
       if (!/[-._:]/.test(ref)) {
-        console.log("hyphen test true")
+        console.log("hyphen test true", ref)
         return true;
       } else {
         console.log("hyphen test false");
@@ -31,11 +32,13 @@
       }
     }
     a.hyphenateNDC = function(ref, hyphen) {
-      a.NDC = a.dehyphenateNDC(ref, hyphen);
+      console.log(ref);
+      // a.NDC = a.dehyphenateNDC(ref);
+      a.NDC = ref;
       a.NDCArr = [];
-      for ( var i = 0; i < 13; i++ ) {
+      for ( var i = 0; i < 12; i++ ) {
         if (i === hyphen.labeler || i === (hyphen.labeler + hyphen.productrx + 1)) {
-          a.NDCArr.push("-")
+          a.NDCArr.push("-");
         } else if (i < hyphen.labeler) {
           a.NDCArr.push(String(a.NDC.charAt(i)));
         } else if (i < hyphen.productrx + hyphen.labeler + 1) {
@@ -44,10 +47,13 @@
           a.NDCArr.push(String(a.NDC.charAt(i - 2)));
         }
       }
+      console.log(a.NDCArr.join(""));
+      ref = a.NDCArr.join("");
+      a.getRxCui(ref);
     }
     a.validateRxCui = function(ref) {
-      a.NDC = ref.ndc_of_associated_covered_drug_or_biological1;
-      var hyphen = [
+      ref = ref.ndc_of_associated_covered_drug_or_biological1;
+      a.hyphen = [
     {
       labeler: 4,
       productrx: 4,
@@ -64,16 +70,10 @@
   package: 2
 }
 ];
-if (a.hyphenTester(ref.ndc_of_associated_covered_drug_or_biological1)) {
-  console.log(ref.ndc_of_associated_covered_drug_or_biological1);
-  // for (var i = 0; i < 3; i++) {
-  //     a.getRxCui(ref, hyphen[i]);
-  //     if (a.rxNorm !== "undefined") {
-  //       break;
-  //     }
-  //   }
-} else {
-  a.preRxCui(ref.ndc_of_associated_covered_drug_or_biological1);
+for (var i = 0; i < 3; i++) {
+  a.dehyphenateNDC(ref);
+  a.hyphenateNDC(a.dehyphenatedNDC, a.hyphen[i]);
+
 }
 // for (var i = 0; i < 3; i++) {
 //   a.getRxCui(ref, hyphen[i]);
@@ -82,10 +82,11 @@ if (a.hyphenTester(ref.ndc_of_associated_covered_drug_or_biological1)) {
 //   }
 // }
 }
-a.getRxCui = function(ref, hyphen) {
-  a.hyphenNDC = a.hyphenateNDC(ref, hyphen);
+a.getRxCui = function(ref) {
+  console.log(ref);
+  // a.hyphenNDC = a.hyphenateNDC(ref, hyphen);
   var url = "http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=";
-  $http.get(url + a.hyphenNDC)
+  $http.get(url + ref)
   .success(function(data) {
     a.rxNorm = data.idGroup.rxnormId;
     if (a.rxNorm !== "undefined") {
