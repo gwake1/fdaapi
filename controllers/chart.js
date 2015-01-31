563f9cb5f61ebde03ddb83af100539b528966702
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-31 11:19:47 -0600

    wip

--- controllers/chart.js
+++ controllers/chart.js
@@ -7,12 +7,12 @@
     rxcui = [ "1191", "3521" ];
     var getdata = function(rxcui) {
       for (var i = 0; i < rxcui.length; i++) {
-        var url = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.rxcui." + rxcui[0] + "&count=patient.reaction.reactionmeddrapt.exact";
+        var url = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.rxcui." + rxcui[i] + "&count=patient.reaction.reactionmeddrapt.exact";
         $http.get(url)
         .success(function(data) {
           console.log(data);
           for (var j = 0; j <= data.results.length; j++) {
-            processEffects(data.results[j], j, data.results.length, i);
+            processEffects(data.results[j], j, data.results.length, i, rxcui.length);
           }
         })
         .error(function(err) {
@@ -20,11 +20,16 @@
         });
       }
     }
-    var processEffects = function(effect, iterate, limit, ver) {
+    var dataName = function(effectsName) {
+      for (var i = 0; i < effectsName.length; i++) {
+
+      }
+    }
+    var processEffects = function(effect, iterate, limit, ver, vertotal) {
       if (iterate == 0) {
         effectsName.push(effect.term);
-        effectsCount.push("data" + ver + 1, effect.count);
-      } else if (iterate === limit) {
+        effectsCount.push("data" + ver, effect.count);
+      } else if (iterate === limit && ver === vertotal) {
         console.log(ver + effectsName);
         console.log(ver + effectsCount);
         var chart = c3.generate({
