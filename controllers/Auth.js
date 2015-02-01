8a5d1a1174e7b7d66ad8030d690edbd9404de8f7
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-31 18:56:21 -0600

    FireBase Auth dependency injection

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,9 +1,18 @@
 (function() {
   "use strict";
-  AuthController.$inject = [ "$http", "$routeParams"];
-  function AuthController($http, $routeParams) {
-
+  AuthController.$inject = [ "$http", "$routeParams" ];
+  function AuthController(firebase, $http, $routeParams) {
+    var a = this,
+    ref = new Firebase("https://docinthelobbyauth.firebaseio.com");
+    function authDataCallback(authData) {
+      if (authData) {
+        console.log("User " + authData.uid + " is logged in with " + authData.provider);
+      } else {
+        console.log("User is logged out");
+      }
+    }
+    ref.onAuth(authDataCallback);
   }
-angular.module("myApp")
-.controller("AuthController", AuthController)
+  angular.module("myApp")
+  .controller("AuthController", AuthController)
 }());
