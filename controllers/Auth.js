36ee36530cae04e13a810ae49482362be065f273
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-31 19:01:49 -0600

    Login of Un-Authenticated Users is Working.

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -4,14 +4,20 @@
   function AuthController(firebase, $http, $routeParams) {
     var a = this,
     ref = new Firebase("https://docinthelobbyauth.firebaseio.com");
-    function authDataCallback(authData) {
-      if (authData) {
-        console.log("User " + authData.uid + " is logged in with " + authData.provider);
+    function authHandler(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
       } else {
-        console.log("User is logged out");
+        console.log("Authenticated successfully with payload:", authData);
       }
     }
-    ref.onAuth(authDataCallback);
+    ref.authWithPassword({
+      email    : "bobtony@firebase.com",
+      password : "correcthorsebatterystaple"
+    }, authHandler);
+    ref.onAuth(function(data) {
+      console.log(data);
+    });
   }
   angular.module("myApp")
   .controller("AuthController", AuthController)
