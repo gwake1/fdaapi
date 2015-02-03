6d17ffcc7f2224614eac5cf3684c8bc85c3e974f
Gerald A. Wakefield <gwake1@gmail.com>
2015-02-03 13:58:04 -0600

    Modified chart.js to create a dynamic array

--- controllers/chart.js
+++ controllers/chart.js
@@ -1,68 +1,121 @@
 (function() {
   // "use strict";
-  function chart ($http) {
-    var
-    effectsName = [],
-    effectsCount = [],
-    rxcui = [ "1191", "3521" ];
-    var getdata = function(rxcui) {
-      for (var i = 0; i < rxcui.length; i++) {
-        var url = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.rxcui." + rxcui[i] + "&count=patient.reaction.reactionmeddrapt.exact";
-        $http.get(url)
-        .success(function(data) {
-          console.log(data);
-          for (var j = 0; j <= data.results.length; j++) {
-            processEffects(data.results[j], j, data.results.length, i, rxcui.length);
-          }
-        })
-        .error(function(err) {
-          console.log(err);
-        });
-      }
-    }
-    var dataName = function(effectsName) {
-      for (var i = 0; i < effectsName.length; i++) {
-
-      }
-    }
-    var processEffects = function(effect, iterate, limit, ver, vertotal) {
-      if (iterate == 0) {
-        effectsName.push(effect.term);
-        effectsCount.push("data" + ver, effect.count);
-      } else if (iterate === limit && ver === vertotal) {
-        console.log(ver + effectsName);
-        console.log(ver + effectsCount);
-        var chart = c3.generate({
-          data: {
-            // xs: {
-            //   "data1": "x1",
-            // },
-            columns: [
-            effectsCount
-            ]
-          },
-          zoom: {
-            enabled: true
-          },
-          axis: {
-            x: {
-              type: "category",
-              categories: effectsName,
-              tick: {
-                rotate: 75,
-                multiline: false
-              },
-              height: 130
-            }
-          }
-        })
-      } else {
-        effectsName.push(effect.term);
-        effectsCount.push(effect.count);
-      }
-    }
-    getdata(rxcui);
+  chart.$inject = [ "$http", "$q" ];
+  function chart($http, $q) {
+    var a = this,
+    gw = []
+    rxcui = [1191, 3521];
+    a.name = rxcui[0].toString() + "rx";
+    json = {};
+    $http.get("https://api.fda.gov/drug/event.json?search=patient.drug.openfda.rxcui." + rxcui[0] + "&count=patient.reaction.reactionmeddrapt.exact")
+    .success(function(data) {
+      rxcui[0] = new Array();
+      gw.push(rxcui[0]);
+      effects = data.results;
+      console.log(effects);
+      $.map(effects, function(n) {
+        var effectscount = n.count;
+        gw[0].push(effectscount);
+      })
+      console.log(gw);
+    })
   }
   angular.module("myApp")
-  .controller  ("chart", chart)
+  .controller("chart", chart)
 }());
+
+
+
+//
+//   function chart ($http) {
+//     var
+//     u ={},
+//     a = [],
+//     effectsName = [],
+//     effectsCount = [],
+//     rxcui = [ "1191", "3521" ];
+//
+//
+//
+//
+//
+//
+//
+//     var getdata = function(rxcui) {
+//       for (var i = 0; i < rxcui.length; i++) {
+//         var url = "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.rxcui." + rxcui[i] + "&count=patient.reaction.reactionmeddrapt.exact";
+//         $http.get(url)
+//         .success(function(data) {
+//           console.log(data);
+//           for (var j = 0; j <= data.results.length; j++) {
+//             processEffects(data.results[j], j, data.results.length, i, rxcui.length);
+//             if (!u.hasOwnProperty(effectsName[j])) {
+//               a.push(effectsName[j]);
+//               u[effectsName[j]] = 1;
+//             }
+//           }
+//           return a;
+//         })
+//         .error(function(err) {
+//           console.log(err);
+//         });
+//       }
+//     }
+//     // var unique = function(arr) {
+//     //   var u = {}, a = [];
+//     //   for(var i = 0, l = arr.length; i < l; ++i){
+//     //     if(!u.hasOwnProperty(arr[i])) {
+//     //       a.push(arr[i]);
+//     //       u[arr[i]] = 1;
+//     //     }
+//     //   }
+//     //   return a;
+//     //   }
+//     // }
+//     var dataName = function(effectsName) {
+//       for (var i = 0; i < effectsName.length; i++) {
+//
+//       }
+//     }
+//     var processEffects = function(effect, iterate, limit, ver, vertotal) {
+//
+//         } else if (iterate === limit && ver === vertotal) {
+//           for (var l = 0; l < effectsCountHolder.length; l++) {
+//             effectsCount.push(effectsCountHolder[l]);
+//             console.log("effects count holder");
+//             console.log(effectsCountHolder);
+//             console.log(effectsCount);
+//           }
+//
+//         } else {
+//           effectsName.push(effect.term);
+//           effectsCountHolder[rxcui[i]].push(effect.count);
+//         }
+//       }
+//     }
+//     var chart = c3.generate({
+//       data: {
+//         json: {
+//           ASPIRIN: [450, 0, 50, 40, 60, 50],
+//           data2: [200, 130, 90, 240, 130, 220],
+//           data3: [300, 200, 160, 400, 250, 250]
+//         }
+//       },
+//       zoom: true,
+//       axis: {
+//         x: {
+//           type: "category",
+//           categories: a,
+//           tick: {
+//             rotate: 75,
+//             multiline: false
+//           },
+//           height: 130
+//         }
+//       }
+//     });
+//     getdata(rxcui);
+//   }
+//   angular.module("myApp")
+//   .controller  ("chart", chart)
+// }());
