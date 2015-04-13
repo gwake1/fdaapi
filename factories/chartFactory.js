5a6e56e16afd21bd9be7be6e880c2b056d1c998f
Gerald A. Wakefield <gwake1@gmail.com>
2015-04-12 20:47:47 -0500

    Successfully returning dynamically created objects via Promises

--- factories/chartFactory.js
+++ factories/chartFactory.js
@@ -2,20 +2,18 @@
   "use strict";
   angular.module("myApp")
   .factory("chartFactory", function($http, $q, $resource) {
-    var drugData = {};
+    var dataResults ={};
     function produceData(drugs){
       var promises = drugs.map(function(drug){
         getDrug(drug);
       });
-      return $q.all(promises);
+      return $q.all(promises).then(function(){return dataResults});
     }
     function getDrug(drugName) {
       var drug =$resource("https://api.fda.gov/drug/event.json?&count=patient.reaction.reactionmeddrapt.exact", {search: '@id'});
       drug.get({search:"patient.drug.openfda.brand_name:"+drugName})
       .$promise.then(function(data) {
-        drugData[drugName] = data.results;
-        console.log(data);
-        console.log(drugData);
+        dataResults[drugName] = data.results;
       });
     }
     return {
