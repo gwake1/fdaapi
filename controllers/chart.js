674b55b0c690a5b6466e7cf8718ea5009e8dd0b6
Gerald A. Wakefield <gwake1@gmail.com>
2015-04-12 23:28:04 -0500

    Unique Terms List via Underscore

--- controllers/chart.js
+++ controllers/chart.js
@@ -2,10 +2,44 @@
   // "use strict";
   chart.$inject = [ "$http", "$q", "$resource", "chartFactory", "$scope" ];
   function chart($http, $q, $resource, chartFactory, $scope) {
+    var minIteration;
     var drugList = ["symbicort", "aggrenox"];
-    $scope.drugData = {};
-    $scope.drugData = chartFactory.produceData(drugList);
-  };
+    var jerry = produceData(drugList);
+    var termList = [];
+    var dataResults = {};
+    $scope.druggies = {};
+    console.log(jerry);
+    function produceData(drugs){
+      var promises = drugs.map(function(drug){getDrug(drug)});
+      minIteration = (drugs.length - 0.5)*100;
+      return $q.all(promises);
+    }
+    function getDrug(drugName) {
+      var drug =$resource("https://api.fda.gov/drug/event.json?&count=patient.reaction.reactionmeddrapt.exact", {search: '@id'});
+      drug.get({search:"patient.drug.openfda.brand_name:"+drugName}, function(data){
+        var results = data.results;
+        dataResults[drugName] = results;
+        $scope.druggies[drugName] = results;
+        console.log(dataResults);
+        makeDupes(results);
+      })
+    }
+    function makeDupes(data){
+      data.map(function(result){
+        var name;
+        name = result.term;
+        termList.push(name);
+        flattenResults();
+      });
+      $scope.terms = termList;
+    }
+    function flattenResults(){
+      if (termList.length > minIteration) {
+        termList = _.uniq(termList);
+        $scope.terms = termList;
+      }
+    }
+  }
   angular.module("myApp")
   .controller("chart", chart)
 }());
--- factories/chartFactory.js
+++ factories/chartFactory.js
@@ -1,24 +1,39 @@
 (function() {
   "use strict";
   angular.module("myApp")
-  .factory("chartFactory", function($http, $q, $resource) {
+  .factory("chartFactory", function($http, $q, $resource, $rootScope) {
     var dataResults ={};
+    function getResults(){
+      return dataResults;
+    }
     function produceData(drugs){
       var promises = drugs.map(function(drug){
         getDrug(drug);
       });
-      return $q.all(promises).then(function(){return dataResults});
+     return $q.all(promises);
     }
     function getDrug(drugName) {
       var drug =$resource("https://api.fda.gov/drug/event.json?&count=patient.reaction.reactionmeddrapt.exact", {search: '@id'});
-      drug.get({search:"patient.drug.openfda.brand_name:"+drugName})
-      .$promise.then(function(data) {
-        dataResults[drugName] = data.results;
-      });
+      drug.get({search:"patient.drug.openfda.brand_name:"+drugName}, function(data){
+        var jerry = data.toJSON();
+        dataResults[drugName] = jerry.results;
+        $rootScope.druggies[drugName] = jerry.results;
+        console.log(dataResults);
+        return dataResults;
+      })
+      // .$promise.then(function(data) {
+      //   return data.results;
+      // }).then(function(results){
+      //   console.log(results);
+      //   dataResults[drugName] = results;
+      //   console.log(dataResults);
+      //   return dataResults;
+      // });
     }
     return {
       getDrug: getDrug,
-      produceData: produceData
+      produceData: produceData,
+      getResults: getResults
     }
   })
 }());
